39ca8ec16f5fe850eadbcabc1234c6062a27db96
anuarmenco <anuarmenco@gmail.com>
2024-10-24 15:45:03 +0000

    adding initial structure

--- Makefile
+++ Makefile
@@ -6,7 +6,9 @@ format:
 	black *.py
     
 lint:
-	pylint --disable=R, C hello.py
+	pylint --disable=R,C hello.py
     
 test:
 	python -m pytest -vv --cov=hello test_hello.py
+
+all: install lint test
--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
+"""Una función que suma 2 variables"""
+
+
 def add(x, y):
+    """Suma 2 variables asigandas"""
     return x + y
-result = add(1, 2)
-print(f"This is the sum: 1 + 2 = {result}")
+
+
+RESULT = add(1, 2)
+print(f"This is the sum: 1 + 2 = {RESULT}")
